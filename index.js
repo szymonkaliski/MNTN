3df39c4f806dc15cb18ac238965721af03e19ac3
Szymon Kaliski <hi@szymonkaliski.com>
2018-02-10 09:27:43 +0100

    basic lump in the center of plane

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const THREE = require("three");
 const OrbitControls = require("three-orbitcontrols");
+const SimplexNoise = require("simplex-noise");
+
+const simplex = new SimplexNoise();
 
 const camera = new THREE.PerspectiveCamera(
   70,
@@ -8,9 +11,7 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-camera.position.x = 3;
-camera.position.y = 3;
-camera.position.z = 3;
+camera.position.z = 1;
 
 // scene
 const scene = new THREE.Scene();
@@ -20,9 +21,23 @@ const ambientLight = new THREE.AmbientLight(0x777777);
 scene.add(ambientLight);
 
 // mesh
-const geometry = new THREE.PlaneGeometry(1, 1, 10, 10);
+const geometry = new THREE.PlaneGeometry(2, 2, 100, 100);
 const material = new THREE.MeshStandardMaterial();
 const mesh = new THREE.Mesh(geometry, material);
+
+console.log(geometry.vertices);
+
+geometry.vertices.forEach(v => {
+  const n = Math.abs(simplex.noise2D(v.x, v.y));
+
+  const mx = Math.sin(Math.PI * (v.x + 0.5));
+  const my = Math.sin(Math.PI * (v.y + 0.5));
+
+  v.z += (n * 0.9) * Math.max(Math.min(mx, my), 0);
+});
+
+mesh.rotation.x = -Math.PI / 2 * 0.8;
+
 scene.add(mesh);
 
 // renderer
