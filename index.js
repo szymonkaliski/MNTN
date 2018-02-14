7606afd6d55f3fa600ce01aa3de3eb4bcd15fed0
Szymon Kaliski <hi@szymonkaliski.com>
2018-02-14 11:09:07 +0100

    updated generation algorithm

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-camera.position.z = 2;
+camera.position.z = 14;
+
+// renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+renderer.setClearColor(0x000000);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+document.body.style.margin = 0;
+document.body.appendChild(renderer.domElement);
 
 // scene
 const scene = new THREE.Scene();
@@ -21,13 +32,14 @@ const ambientLight = new THREE.AmbientLight(0x777777);
 scene.add(ambientLight);
 
 const pointLight = new THREE.PointLight(0xffffff, 1, 0);
-pointLight.position.set(0, 100, 0);
+pointLight.position.set(0, 100, 10);
+pointLight.castShadow = true;
 scene.add(pointLight);
 
 // mesh
-const planeWidth = 4;
-const planeHeight = 4;
-const planeSteps = 200;
+const planeWidth = 10;
+const planeHeight = 10;
+const planeSteps = 100;
 const geometry = new THREE.PlaneGeometry(
   planeWidth,
   planeHeight,
@@ -35,15 +47,36 @@ const geometry = new THREE.PlaneGeometry(
   planeSteps
 );
 
-const nMod = 1.9
+const nMod = 10;
+const heightMod = 0.2;
+const neighbourMod = 0.5;
+const mountainCenter = new THREE.Vector2(0, 4);
+const mountainR = 5;
 
 geometry.vertices.forEach(v => {
-  const n = Math.abs(simplex.noise2D(v.x / planeWidth * nMod, v.y / planeHeight * nMod));
+  const n = Math.abs(
+    simplex.noise2D(v.x / planeWidth * nMod, v.y / planeHeight * nMod)
+  );
 
-  const mx = Math.sin(Math.PI * (v.x / planeWidth + 0.5));
-  const my = Math.sin(Math.PI * (v.y / planeHeight + 0.5));
+  const posxy = new THREE.Vector2(v.x, v.y);
+  const sizeMod =
+    1 - Math.min(mountainCenter.distanceTo(posxy) / mountainR, 1) + 0.1;
 
-  v.z += n * 0.9 * Math.max(Math.min(mx, my), 0);
+  v.noise = n * sizeMod * heightMod;
+});
+
+geometry.vertices.forEach(v => {
+  const neighbours = geometry.vertices.filter(v2 => {
+    const v1xy = new THREE.Vector2(v.x, v.y);
+    const v2xy = new THREE.Vector2(v2.x, v2.y);
+
+    const dist = v1xy.distanceTo(v2xy);
+
+    return dist < neighbourMod && dist > 0.0001;
+  });
+
+  v.z +=
+    v.noise + neighbours.reduce((memo, { noise }) => memo + Math.abs(noise), 0);
 });
 
 geometry.computeBoundingSphere();
@@ -54,20 +87,16 @@ const material = new THREE.MeshStandardMaterial({
   color: 0x222222,
   emissive: 0x010101,
   roughness: 0.7,
-  metalness: 0.2
+  metalness: 0.2,
+  flatShading: true
 });
 const mesh = new THREE.Mesh(geometry, material);
 
 mesh.rotation.x = -Math.PI / 2 * 0.8;
+mesh.castShadow = true;
+mesh.receiveShadow = true;
 
 scene.add(mesh);
-
-// renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setClearColor(0x000000);
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.style.margin = 0;
-document.body.appendChild(renderer.domElement);
 
 // orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
