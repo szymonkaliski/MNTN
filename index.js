53f146e2d8fd64560bb4683d5b0d120528609997
Szymon Kaliski <hi@szymonkaliski.com>
2018-03-08 08:56:12 +0100

    better camera movement

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ require("three/examples/js/postprocessing/FilmPass");
 const ui = new Gui({ size: 300 });
 
 let opts = {
-  postProcessingEnabled: true,
+  postProcessingEnabled: false,
 
   onlyAO: false,
   radius: 32,
@@ -89,6 +89,36 @@ const camera = new THREE.PerspectiveCamera(
 
 camera.position.z = 10;
 
+const offset = new THREE.Vector3();
+
+const rotateEnd = new THREE.Vector2();
+const rotateStart = new THREE.Vector2();
+const rotateDelta = new THREE.Vector2();
+
+const spherical = new THREE.Spherical();
+const sphericalDelta = new THREE.Spherical();
+
+const rotateSpeed = 0.2;
+
+const onMouseMove = () => {
+  rotateEnd.set(
+    event.clientX - window.innerWidth / 2,
+    event.clientY - window.innerHeight / 2
+  );
+
+  rotateDelta.subVectors(rotateEnd, rotateStart);
+
+  sphericalDelta.theta -=
+    2 * Math.PI * rotateDelta.x / window.innerWidth * rotateSpeed;
+
+  sphericalDelta.phi -=
+    2 * Math.PI * rotateDelta.y / window.innerHeight * rotateSpeed;
+
+  rotateStart.copy(rotateEnd);
+};
+
+window.addEventListener("mousemove", onMouseMove, false);
+
 // renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
@@ -124,9 +154,9 @@ scene.add(bgSphereMesh);
 
 // mesh
 const planeWidth = 20;
-const planeHeight = 10;
+const planeHeight = 20;
 const planeStepsWidth = 100;
-const planeStepsHeight = 50;
+const planeStepsHeight = 100;
 const geometry = new THREE.PlaneGeometry(
   planeWidth,
   planeHeight,
@@ -220,14 +250,57 @@ const filmPass = new THREE.FilmPass(
 filmPass.renderToScreen = true;
 effectComposer.addPass(filmPass);
 
-// orbit controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = false;
-controls.enableZoom = true;
+// // orbit controls
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = false;
+// controls.enableZoom = true;
 
 // loop
+const quat = new THREE.Quaternion().setFromUnitVectors(
+  camera.up,
+  new THREE.Vector3(0, 1, 0)
+);
+const quatInverse = quat.clone().inverse();
+const target = new THREE.Vector3();
+
+const SPHERICAL_PHI_MAX = 1.75;
+const SPHERICAL_PHI_MIN = 1.54;
+const SPHERICAL_THETA_MAX = 0.4;
+const SPHERICAL_THETA_MIN = -0.4;
+const CAMERA_DAMPING = 0.2;
+const CAMERA_UPDATE_K = 0.05;
+
 const loop = () => {
   requestAnimationFrame(loop);
+
+  offset.copy(camera.position).sub(target);
+  offset.applyQuaternion(quat);
+
+  spherical.setFromVector3(offset);
+  spherical.theta += sphericalDelta.theta * CAMERA_UPDATE_K;
+  spherical.phi += sphericalDelta.phi * CAMERA_UPDATE_K;
+  spherical.radius = 10;
+
+  spherical.phi = Math.max(
+    SPHERICAL_PHI_MIN,
+    Math.min(SPHERICAL_PHI_MAX, spherical.phi)
+  );
+
+  spherical.theta = Math.max(
+    SPHERICAL_THETA_MIN,
+    Math.min(SPHERICAL_THETA_MAX, spherical.theta)
+  );
+
+  spherical.makeSafe();
+
+  offset.setFromSpherical(spherical);
+  offset.applyQuaternion(quatInverse);
+
+  camera.position.copy(target).add(offset);
+  camera.lookAt(target);
+
+  sphericalDelta.theta *= 1 - CAMERA_DAMPING;
+  sphericalDelta.phi *= 1 - CAMERA_DAMPING;
 
   pointLight.position.set(opts.lightX, opts.lightY, opts.lightZ);
 
@@ -268,4 +341,3 @@ const onWindowResize = () => {
 };
 
 window.addEventListener("resize", onWindowResize, false);
-
